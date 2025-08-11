ebef894fe9343a3234a3d1b49bded88f15abade4
shubham-covrzy <shubham@covrzy.com>
2025-08-11 13:18:38 +0530

    added .env.example

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ load_dotenv()  # load .env variables
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    API_BASE_URL_US = os.getenv("API_BASE_URL_US", "https://financialmodelingprep.com/api/v3")
-    API_KEY_US = os.getenv("API_KEY_US", "5nR14kK8fixktAEey5CLd7s4pvE1mWRN")
+    API_BASE_URL_US = os.getenv("API_BASE_URL_US", "<fall-back-us-url>")
+    API_KEY_US = os.getenv("API_KEY_US", "<fall-back--us-key>")
 
     # Database configuration
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'app.db'))
